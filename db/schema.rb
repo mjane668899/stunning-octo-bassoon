4a63f1e569eeb9a6eac80d7f0b478965af75beb4
Michelle Teoh Sue Mae <mjane668899@gmail.com>
2020-11-06 15:33:04 +1100

    Added stripe_user_id:string to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_022847) do
+ActiveRecord::Schema.define(version: 2020_11_06_043119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_022847) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_user_id"
   end
 
 end
