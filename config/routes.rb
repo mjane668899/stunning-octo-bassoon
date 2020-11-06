9b999ae413118dfdff508e5e4550dc82e50b5017
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-06 14:54:52 +1100

    stripe routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # stripe : create connect accounts
+  scope '/account' do
+    post 'create', to: 'account#create', as: 'account_create'
+    get 'refresh', to: 'account#refresh', as: 'account_refresh'
+    get 'returns', to: 'account#returns', as: 'account_returns'
+    post 'dashboard', to: 'account#stripe_dashboard', as: 'stripe_dashboard'
+  end
+
 end
