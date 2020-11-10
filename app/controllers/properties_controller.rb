4782a39167465e92e18e8cdfb679e0fe583eccdf
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-10 11:39:00 +1100

    update properties show

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -45,6 +45,6 @@ class PropertiesController < ApplicationController
 
   private
   def property_params
-    params.require(:property).permit(:title, :price, :description, :street_number, :address, :suburb, :state, :country, :guest, :bedroom, :bathroom, :bed, :parking_space, :available_date, :photo, :details)
+    params.require(:property).permit(:title, :price, :description, :street_number, :address,  :guest, :bedroom, :bathroom, :bed, :parking_space, :available_date)
   end
 end
