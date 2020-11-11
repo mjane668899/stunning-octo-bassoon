96933a760e5d6c2fde172a804ee97b3c24482529
JayOverall <jay.overall@hotmail.com>
2020-11-11 16:02:21 +1100

    update booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,8 +14,29 @@ class BookingsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @booking = Booking.find params[:id]
+  end
+
+  def edit
+    @booking = Booking.find params[:id]
+  end
+
+  def update
+    booking = Booking.find params[:id]
+    booking.update booking_params
+    redirect_to booking
+  end
+
+  def destroy
+    booking = Booking.find params[:id]
+    booking.destroy
+    redirect_to root_path
+  end
+
+  private
   def booking_params
-    params.require(:booking).permit(:title)
+    params.require(:booking).permit(:start_date, :end_date, :nights, :total_price, :guests, :user_id, :property_id)
   end
 
 end
