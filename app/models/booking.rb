84681c578534496ae344f2f5644d908fa49a82c7
JayOverall <jay.overall@hotmail.com>
2020-11-11 13:22:37 +1100

    Wednesday changes- Jasper

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,17 +3,17 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, :num_guests, presence: true,
   reservation: true
 
-      belongs_to: (:property)
-      class_name: (:property)
+      belongs_to: :property
+      class_name: :property
       # foreign_key: :spot_id
 
-      belongs_to: (:guest)
-      class_name: (:user)
-      foreign_key: (:guest_id)
+      belongs_to: :guest
+      class_name: :user
+      foreign_key: :guest_id
 
-      has_one (:user)
-      through: (:property)
-      source: (:user)
+      has_one :user
+      through: :property
+      source: :user
 
       booking.create(reservation:'start_date','end_date')=>.valid # => true
       booking.create(reservation: nil)=>.valid? # => false
